acdb69e4276c09cf2424a2e13faf20a231806273
M Wahyu Ayesha <mine215567@gmail.com>
2025-04-22 09:22:53 +0700

    Memperbaiki konflik dan memperbarui resource file

--- android/app/src/main/kotlin/com/example/newsapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/newsapp/MainActivity.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.example.newsapp
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-=======
-package com.example.newsapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> fitur/simpan-photo-profile
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 allprojects {
     repositories {
         google()
@@ -20,26 +19,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-=======
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
->>>>>>> fitur/simpan-photo-profile
